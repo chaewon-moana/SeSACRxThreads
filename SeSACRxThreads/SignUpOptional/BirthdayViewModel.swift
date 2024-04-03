1075bfb5db49a5f186bfa8e4bd3b13848773908c
chaewon-moana <dreamytaurus508@gmail.com>
2024-04-03 10:19:59 +0900

    birthday viewModel 분리전

---
+++ SeSACRxThreads/SignUpOptional/BirthdayViewModel.swift
@@ -0,0 +1,20 @@
+//
+//  BirthdayViewModel.swift
+//  SeSACRxThreads
+//
+//  Created by cho on 4/3/24.
+//
+
+import Foundation
+
+class BirthdayViewModel {
+    
+    //input -> datePicker에서 선택할 날짜(date type)
+    
+    //output -> label (year, month, day)
+    
+    
+    
+    
+    
+}
